cb256f91208b09f1543117b106898e69957ff777
HurleyJ44 <jfhurley@uvm.edu>
2016-10-15 02:27:52 -0400

    Created the main.go file that will run the server code.

--- src/main.go
+++ src/main.go
@@ -1,9 +1,53 @@
 package main
 
-import "net/http"
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+    "database/sql"
+
+     _"https://www.github.com/mattn/go-sqlite3"
+)
+
+type Libraries struct {
+  ID      int
+  Name    string
+  Town    string
+  Mailing string
+  Street  string
+  Phone   string
+  Web     string
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func InitDB(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+	if err != nil { panic(err) }
+	if db == nil { panic("db nil") }
+	return db
+}
+
+func CreateTable(db *sql.DB) {
+	// create table if not exists
+	sql_table := `
+	CREATE TABLE IF NOT EXISTS items(
+		Id TEXT NOT NULL PRIMARY KEY,
+		Name TEXT,
+		Phone TEXT,
+		InsertedDatetime DATETIME
+	);
+	`
+
+	_, err := db.Exec(sql_table)
+	if err != nil { panic(err) }
+}
 
 func main() {
-  fmt.Println("Hello World!")
 
+}
+
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
 }
